d8669c775ec29234e6d271b270ffdff93a308504
Felipe Seabra <feliperoce@gmail.com>
2022-10-24 18:28:21 -0300

    Requisito 7

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,20 +3,23 @@ import { Switch, Route } from 'react-router-dom';
 import Meals from '../pages/Meals';
 import Login from '../pages/Login';
 import Drinks from '../pages/Drinks';
+import Profile from '../pages/Profile';
+import DoneRecipes from '../pages/DoneRecipes';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
 
 export default class Content extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/favorite-recipes" />
         <Route path="/drinks/:id-da-receita/in-progress" />
         <Route path="/meals/:id-da-receita/in-progress" />
         <Route path="/drinks/:id-da-receita" />
         <Route path="/meals/:id-da-receita" />
-        <Route path="/profile" />
+        <Route path="/profile" component={ Profile } />
         <Route path="/drinks" component={ Drinks } />
         <Route path="/meals" component={ Meals } />
-        <Route path="/done-recipes" />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
         <Route exact path="/" component={ Login } />
       </Switch>
     );
