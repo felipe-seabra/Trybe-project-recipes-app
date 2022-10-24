b16fdbf3ee0b966393c0de07ec14d7b1ca73a46d
Victor Mendes <victorutdsim7@gmail.com>
2022-10-24 17:21:25 -0300

    rotas sem components

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen, waitFor, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import App from '../App';
 
 describe('Testando o componente Login', () => {
+  const validPassword = 'abc1234';
+  const validEmail = 'abc@hotmail.com';
   it('Verificando se a pagina login tem o comportamento desejado', async () => {
     const { history } = renderWithRouterAndRedux(<App />);
     expect(history.location.pathname).toBe('/');
@@ -15,14 +17,35 @@ describe('Testando o componente Login', () => {
     expect(emailInput).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
 
-    userEvent.type(password, 'abc1234');
-    userEvent.type(emailInput, 'abc@hotmail.com');
-    expect(password.value).toBe('abc1234');
-    expect(emailInput.value).toBe('abc@hotmail.com');
+    userEvent.type(password, validPassword);
+    userEvent.type(emailInput, validEmail);
+    expect(password.value).toBe(validPassword);
+    expect(emailInput.value).toBe(validEmail);
     expect(loginButton).toBeEnabled();
     userEvent.click(loginButton);
     await waitFor(() => {
       expect(history.location.pathname).toBe('/meals');
     }, { timeout: 5000 });
+  });
+
+  it('Verifica se as informações são salvas na store', () => {
+    const { store } = renderWithRouterAndRedux(<App />);
+    const password = screen.getByTestId('password-input');
+    const emailInput = screen.getByTestId('email-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
+    act(() => {
+      userEvent.type(password, validPassword);
+      userEvent.type(emailInput, validEmail);
+      userEvent.click(loginButton);
+    });
+    const {
+      login: {
+        password: statePassword,
+        email: stateEmail,
+      },
+    } = store.getState();
+
+    expect(stateEmail).toEqual(validEmail);
+    expect(statePassword).toEqual(validPassword);
   });
 });
