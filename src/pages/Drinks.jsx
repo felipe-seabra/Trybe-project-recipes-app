4949131ff99218358d3a99f5c9691f43de20bb20
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-10-27 15:11:16 -0300

    Renderiza no drinks

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ButtonFilter from '../components/ButtonFilter';
 
 function Drinks() {
   return (
     <div>
       <Header title="Drinks" />
+      <ButtonFilter />
       <Footer />
     </div>
   );
