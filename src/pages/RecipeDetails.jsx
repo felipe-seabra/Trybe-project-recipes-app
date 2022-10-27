0cde70c420ffad471ae91ce945d0d18b84d81882
Felipe Seabra <feliperoce@gmail.com>
2022-10-27 20:01:02 -0300

    Mais um pouco do requisito 24

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,20 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import RecipeDetailsApi from '../services/RecipeDetailsApi';
 
 function RecipeDetails({ history }) {
+  const { location: { pathname } } = history;
+  const [parameters, setParameters] = useState([]);
+
+  const verifyPathname = (categoryApi) => {
+    console.log(categoryApi);
+    if (pathname === '/drinks') {
+      const { strDrink, strDrinkThumb, idDrink, strCategory,
+      } = categoryApi[0];
+      const data = {
+        instruction: strInstructions,
+        category: strCategory,
+        picture: strDrinkThumb,
+        title: strDrink,
+        id: idDrink,
+      };
+      setParameters(data);
+    } else {
+      const { strMeal, strMealThumb, idMeal, strCategory, strInstructions, strYoutube,
+      } = categoryApi[0];
+      const data = {
+        video: strYoutube,
+        instruction: strInstructions,
+        category: strCategory,
+        picture: strMealThumb,
+        title: strMeal,
+        id: idMeal,
+      };
+      setParameters(data);
+    }
+    // console.log(parameters);
+  };
+
   useEffect(() => {
     const handleFilter = async () => {
-      const { location: { pathname } } = history;
       const categoryApi = await RecipeDetailsApi('52977', pathname);
-      console.log(categoryApi);
+      verifyPathname(categoryApi);
     };
     handleFilter();
-  }, []);
+  }, [history]);
 
   return (
-    <div>test</div>
+    <div className="container">
+      <img
+        data-testid="recipe-photo"
+        src={ parameters.picture }
+        alt={ parameters.title }
+      />
+      <h1 data-testid="recipe-title">{parameters.title}</h1>
+      <p data-testid="recipe-category">{parameters.category}</p>
+      <p data-testid="instructions">{parameters.instruction}</p>
+      {
+        (pathname === `/meals/${parameters.id}`)
+        && <iframe
+          data-testid="video"
+          title="youtube video"
+          src={ parameters.video.replace('watch?v=', 'embed/') }
+          frameBorder="0"
+          allowFullScreen
+          width="100%"
+        />
+      }
+    </div>
   );
 }
 
