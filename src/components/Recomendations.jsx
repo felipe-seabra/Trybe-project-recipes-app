05b64e68a8c77362c295b0813cefed8b5894c091
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-10-28 14:32:20 -0300

    update da 26

--- src/components/Recomendations.jsx
+++ src/components/Recomendations.jsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import getMeal from '../services/mealApi';
+// import getMeal from '../services/mealApi';
 
 function Recomendations({ history }) {
-  // const [response, setResponse] = useState([]);
-  const [mealApi, setMealApi] = useState([]);
+  const [recomendationData, setRecomendationData] = useState([]);
+  // const [mealApi, setMealApi] = useState([]);
   // const [drinkApi, setDrinkApi] = useState([]);
 
   const { location: { pathname } } = history;
-  useEffect(() => {
-    const recipe = async () => {
-      const awaitMeal = await getMeal('Name', '', pathname);
-      setMealApi(awaitMeal);
-    };
-    recipe();
-  }, [history, pathname]);
+  const getRecomendationApi = async () => {
+    if (pathname === '/drinks') {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const data = await response.json();
+      setRecomendationData(data);
+    } else {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const data = await response.json();
+      setRecomendationData(data);
+    }
+  };
 
-  console.log(mealApi);
+  console.log(recomendationData);
+
+  useEffect(() => {
+    getRecomendationApi();
+  }, []);
 
   return (
     <div>alo</div>
