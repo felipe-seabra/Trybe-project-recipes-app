a97fa2dc64768d5b451195d61dad188fa2b912c7
Felipe Seabra <feliperoce@gmail.com>
2022-10-28 14:54:01 -0300

    setup context

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState, useMemo } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Recipes from '../components/Recipes';
 
-class Meals extends Component {
-  render() {
-    return (
-      <div>
-        <Header title="Meals" />
-        <Recipes />
-        <Footer />
-      </div>
-    );
-  }
+function Meals() {
+  const [search, setSearch] = useState(false);
+
+  const handleSearch = useMemo(() => ({
+    setSearch,
+  }), [setSearch]);
+
+  return (
+    <div>
+      <Header title="Meals" />
+      {!search
+        && <Recipes />}
+      <Footer />
+    </div>
+  );
 }
 
 export default Meals;
