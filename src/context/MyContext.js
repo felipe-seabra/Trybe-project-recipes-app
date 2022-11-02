d192f3a89852f7633376ab2a77c6ffde9accf101
Victor Mendes <victorutdsim7@gmail.com>
2022-11-02 14:11:45 -0300

    requisito 42

--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useMemo } from 'react';
+import React, { createContext, useState, useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import copy from 'clipboard-copy';
 
@@ -7,6 +7,11 @@ export const Mycontext = createContext();
 function ContextProvider({ children }) {
   const [search, setSearch] = useState(false);
   const [shareCopy, setShareCopy] = useState([]);
+  const [parameters, setParameters] = useState([]);
+  const [ingredientsAndMeasures, setIngredientsAndMeasure] = useState({
+    ingredients: [],
+    measures: [],
+  });
 
   const handleCopy = () => {
     const url = window.location.href;
@@ -25,12 +30,54 @@ function ContextProvider({ children }) {
     }, THREE_SECONDS);
   };
 
+  const separateIngredientsAndMeasures = (obj) => {
+    const entries = Object.entries(obj);
+    const extractIngredientsAndMeasure = entries.reduce((acc, element) => {
+      const accCopy = { ...acc };
+      const key = element[0];
+      const value = element[1];
+      if (key.includes('Ingredient') && value) {
+        accCopy.ingredients.push(value);
+      }
+      if (key.includes('Measure') && (value !== ' ')) {
+        accCopy.measures.push(value);
+      }
+      return accCopy;
+    }, {
+      ingredients: [],
+      measures: [],
+    });
+    setIngredientsAndMeasure(extractIngredientsAndMeasure);
+  };
+  const verifyPathname = useCallback((categoryApi) => {
+    separateIngredientsAndMeasures(categoryApi[0]);
+    const {
+      strDrink, strDrinkThumb, idDrink, strCategory, strInstructions, strAlcoholic,
+      strYoutube, idMeal, strMeal, strMealThumb,
+    } = categoryApi[0];
+
+    const data = {
+      video: strYoutube,
+      instruction: strInstructions,
+      alcohol: strAlcoholic,
+      category: strCategory,
+      picture: strDrinkThumb || strMealThumb,
+      title: strDrink || strMeal,
+      id: idDrink || idMeal,
+    };
+
+    setParameters(data);
+  }, []);
+
   const contextValue = useMemo(() => ({
     search,
     shareCopy,
     handleCopy,
     setSearch,
-  }), [search, shareCopy]);
+    verifyPathname,
+    parameters,
+    ingredientsAndMeasures,
+  }), [search, shareCopy, parameters, verifyPathname, ingredientsAndMeasures]);
 
   return (
     <Mycontext.Provider value={ contextValue }>
